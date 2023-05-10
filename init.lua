2623c8f3289b9c5a5126f1a4404fcac2368b5dbf
Warren Armstrong <warrentarmstrong@gmail.com>
2023-05-09 22:56:06 -0400

    update init

--- init.lua
+++ init.lua
@@ -1,1 +1,20 @@
-print("this is init")
+function get_contents(branch)
+    pathRes = http.get("https://pdhaambzdi.execute-api.us-east-1.amazonaws.com/" .. branch)
+    paths = pathRes.readAll()
+
+    for path in string.gmatch(paths, "([^,]+)") do
+        print("Downloading: " .. path)
+        contentRes = http.get("https://raw.githubusercontent.com/WarrenArmstrong/cc-sync/" .. branch .. "/" .. path)
+        content = contentRes.readAll()
+
+        f = fs.open(path, "w")
+        f.write(content)
+        f.close()
+    end
+end
+
+local arg1, = ...
+
+if arg1 != nil
+    get_cotnets(arg1)
+end
